3fed0c8150ba479b1a899bafe4c8ea6df958b010
Srivatsan Srinivasan <srivat97@outlook.com>
2019-11-04 18:29:07 -0500

    added basic project structure

---
+++ account.c
@@ -0,0 +1,5 @@
+//
+// Created by Srivatsan on 2019-11-04.
+//
+
+#include "account.h"
---
+++ account.h
@@ -0,0 +1,21 @@
+//
+// Created by Srivatsan on 2019-11-04.
+//
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif ACCOUNT_H
